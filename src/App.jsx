b201357a47cbd83265d78dee7486ea392b6d2baf
josh-a-bant <jashobantbesra@gmail.com>
2025-11-27 23:46:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-black text-cyan-500 py-2 px-4">
+        Akrabhi Assignment
+      </h1>
+    </>
+  );
+}
+
+export default App;

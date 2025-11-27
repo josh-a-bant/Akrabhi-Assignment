919688eb74b72dd21ef98cb7a811e5600fc327c6
josh-a-bant <jashobantbesra@gmail.com>
2025-11-28 00:42:38 +0530

    complete header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl bg-black text-cyan-500 py-2 px-4">
-        Akrabhi Assignment
-      </h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { IconX } from "@tabler/icons-react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center min-h-[76px] border border-[#E4E4E7]">
+      <div className=" flex gap-0.5 text-lg font-medium text-[#09090B]">
+        <div className="py-6 px-9 border-b-2 border-[#AF52DE] active:bg-[#F1F5F9CC]">
+          New Patient Registration
+        </div>
+        <div className="py-6 px-9 flex gap-2 text-lg active:bg-[#F1F5F9CC] items-center">
+          <span className="rounded-xl size-6 bg-[#007AFF] flex justify-center items-center text-[#FFFFFF]">
+            1
+          </span>
+          Incoming ABHA Consent
+        </div>
+      </div>
+      <div className="p-2 bg-[#F4F4F5] rounded-xs mr-5">
+        <IconX stroke={1} className="" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;

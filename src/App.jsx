adc740688b560b6ccfcfa8fe64657eddc66c0333
josh-a-bant <jashobantbesra@gmail.com>
2025-11-28 02:20:03 +0530

    modify some css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Middle from "./components/Middle";
 
 function App() {
   return (
-    <>
+    <div className="relative h-screen max-w-7xl m-auto border border-gray-100">
       <Header />
-      <Main />
-    </>
+      <Middle />
+      <Footer />
+    </div>
   );
 }
 

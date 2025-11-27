def0bbf34f20fd720f878a5e3bfc44dd44310e4a
josh-a-bant <jashobantbesra@gmail.com>
2025-11-28 01:39:05 +0530

    complete row part

---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+const Main = () => {
+  return (
+    <main className="px-6 mt-3h">
+      {/* Row */}
+      <div className="py-2 max-w-md">
+        <div className="flex justify-around items-center p-1 bg-[#F1F5F9] text-sm font-medium rounded-lg">
+          <div className="rounded-sm py-1 px-3 bg-[#FFFFFF] text-[#020817]">
+            Regular
+          </div>
+          <div className="rounded-sm py-1 px-3 text-[#64748B]">Quick</div>
+          <div className="rounded-sm py-1 px-3 text-[#64748B]">
+            Import from ABHA
+          </div>
+          <div className="rounded-sm py-1 px-3 text-[#64748B]">
+            Scan Documents
+          </div>
+        </div>
+      </div>
+
+      {/* todo */}
+      {/* <div></div> */}
+    </main>
+  );
+};
+
+export default Main;

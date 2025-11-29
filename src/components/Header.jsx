c61cf63df678900452245e73fbfa134a8828b514
josh-a-bant <jashobantbesra@gmail.com>
2025-11-30 03:40:32 +0530

    fixing the style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,22 @@ import { IconX } from "@tabler/icons-react";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center min-h-[76px] border-b border-[#E4E4E7] w-full">
+    <header className="flex justify-between items-center h-[76px] border border-[#E4E4E7] w-full">
       <div className=" flex gap-0.5 text-lg font-medium text-[#09090B]">
         <div className="py-6 px-9 border-b-2 border-[#AF52DE] bg-[#F1F5F9CC]">
           New Patient Registration
         </div>
-        <div className="py-6 px-9 flex gap-2 text-lg items-center">
-          <span className="rounded-xl size-6 bg-[#007AFF] flex justify-center items-center text-[#FFFFFF]">
+
+        <div className="p-6 flex gap-2 items-center border-b border-[#E2E8F0] ">
+          <span className="rounded-xl size-6 bg-[#007AFF] flex justify-center items-center text-[11px] text-[#FFFFFF]">
             1
           </span>
-          Incoming ABHA Consent
+          <p>Incoming ABHA Consent</p>
         </div>
       </div>
-      <div className="p-2 bg-[#F4F4F5] rounded-xs mr-5 cursor-pointer">
-        <IconX stroke={1} className="" />
+
+      <div className="bg-[#F4F4F5] rounded-xs mr-5 cursor-pointer size-10 flex justify-center items-center">
+        <IconX stroke={1} className="size-6" />
       </div>
     </header>
   );
